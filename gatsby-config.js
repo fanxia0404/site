967f16690b8764aa583ca117768b49b17a02d641
Fan <fxia@salesforce.com>
2020-05-10 21:30:10 -0700

    cleaned up files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   pathPrefix: "/site",
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Fan Xia`,
+    description: `Fan Xia's personal site`,
+    author: `@fanxia0404`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
