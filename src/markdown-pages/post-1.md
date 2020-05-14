88bec0301168d886dc213efa9601a6b251a3d1b5
Fan Xia <5935836+fanxia0404@users.noreply.github.com>
2020-05-14 00:35:35 -0700

    render markdown (#2)

---
+++ src/markdown-pages/post-1.md
@@ -0,0 +1,9 @@
+---
+slug: "/posts/post-1"
+date: "2020-05-14"
+title: "My first blog post"
+---
+
+# My First Blog Post
+
+Hello World!
---
+++ src/templates/postTemplate.js
@@ -0,0 +1,35 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+import '../pages/index.scss'
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+export default function PostTemplate({
+  data, // this prop will be injected by the GraphQL query below.
+}) {
+  const { markdownRemark } = data // data.markdownRemark holds your post data
+  const { frontmatter, html } = markdownRemark
+  return (
+    <Layout>
+      <SEO title={frontmatter.title} />
+      <div
+          className="blog-post-content"
+          dangerouslySetInnerHTML={{ __html: html }}
+        />
+    </Layout>
+  )
+}
+
+export const pageQuery = graphql`
+  query($slug: String!) {
+    markdownRemark(frontmatter: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        slug
+        title
+      }
+    }
+  }
+`
