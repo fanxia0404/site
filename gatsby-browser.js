afce66cc783291894f024b8501210edd936d1f8d
Fan <fxia@salesforce.com>
2020-05-16 00:46:53 -0700

    move GA to browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,4 +4,13 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
-// You can delete this file if you're not using it
+import ReactGA from 'react-ga';
+
+export const onClientEntry = () => {
+    // Initialize GA
+    ReactGA.initialize('UA-166849405-1');
+}
+
+export const onRouteUpdate = ({ location, prevLocation }) => {
+    ReactGA.pageview(location.pathname + location.search);
+}
