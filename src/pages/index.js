9efbd939a6e053427a85f2bccbf9406811d1fcab
Fan Xia <5935836+fanxia0404@users.noreply.github.com>
2020-05-16 00:47:32 -0700

    move GA to browser.js (#7)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
-import React, { useEffect } from "react"
+import React from "react"
 import { graphql } from "gatsby"
-import ReactGA from 'react-ga';
 import './styles.scss'
 
 import PostLink from "../components/postLink"
@@ -12,12 +11,6 @@ const IndexPage = ({
     allMarkdownRemark: { edges },
   },
 }) => {
-  useEffect(() => {
-    // Initialize 
-    ReactGA.initialize('UA-166849405-1');
-    ReactGA.pageview(window.location.pathname + window.location.search);
-  });
-
   const Posts = edges
     .filter(edge => !!edge.node.frontmatter.date) // You can filter your posts based on some criteria
     .map(edge => (
