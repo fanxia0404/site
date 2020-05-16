7873287d8f259e87fe75e3f821c7cc32b454d78d
Fan <fxia@salesforce.com>
2020-05-16 12:51:27 -0700

    compress video

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,23 +9,26 @@ import SnowboardVideo from "../assets/snowboard_park.mp4"
 import DeerVideo from "../assets/deer.mp4"
 
 const AboutPage = () => {
-    const Videos = [SnowboardVideo, DogVideo, DeerVideo].map((video, index) => (
-        <div style={{
-            height: "100vh",
-            width: "100vw",
-            backgroundColor:  index % 2 == 0 ? "#ffffff" : "#24292e",
-            color:  index % 2 == 0 ? "#24292e" : "#ffffff"
-        }}>
+    const Videos = [ DogVideo, SnowboardVideo, DeerVideo ].map((video, index) => (
+        <div
+            key={index}
+            style={{
+                height: "100vh",
+                width: "100vw",
+                backgroundColor:  index % 2 ? "#24292e" : "#ffffff",
+                color:  index % 2 ? "#ffffff" : "#24292e"
+            }}
+        >
             <video
                 playsInline 
                 autoPlay 
                 loop 
-                muted 
+                muted
                 style={{ height: "100%", width: "100%", objectFit: "cover" }}
             >
                 <source src={video} type="video/mp4" />
-                <div class="blankslate">
-                    <h3 class="mb-1">Your browser doesn't support HTML5 video.</h3>
+                <div className="blankslate">
+                    <h3 className="mb-1">Your browser doesn't support HTML5 video.</h3>
                     <p>Here is a <a href={video}>link to the video</a> instead.</p>
                 </div>
             </video>
