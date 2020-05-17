00744115ff66ecc02d39b49aefc69d792b7f0d77
Fan Xia <5935836+fanxia0404@users.noreply.github.com>
2020-05-17 14:01:58 -0700

    add offline support (#13)

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,7 +41,7 @@ module.exports = {
                 start_url: `/`,
                 background_color: `#663399`,
                 theme_color: `#663399`,
-                display: `minimal-ui`,
+                display: `standalone`,
                 icon: `src/assets/images/avatar.png`, // This path is relative to the root of the site.
             },
         },
@@ -53,6 +53,6 @@ module.exports = {
         },
         // this (optional) plugin enables Progressive Web App + Offline functionality
         // To learn more, visit: https://gatsby.dev/offline
-        // `gatsby-plugin-offline`,
+        `gatsby-plugin-offline`,
     ],
 }
