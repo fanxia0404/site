a5ddaafaac63f453c0278d0e460e99ae15d277ae
Fan <fxia@salesforce.com>
2020-05-17 10:54:57 -0700

    auto populate post slug and date

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,6 +4,28 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
+const { createFilePath } = require(`gatsby-source-filesystem`)
+
+exports.onCreateNode = ({ node, getNode, actions }) => {
+    const { createNodeField } = actions
+    if (node.internal.type === `MarkdownRemark`) {
+        // Auto populate date field from file name
+        createNodeField({
+            node,
+            name: `date`,
+            value: getNode(node.parent).name.substring(0, 10),
+        })
+
+        // Auto populate slug
+        const relativePath = createFilePath({ node, getNode, basePath: `posts` })
+        createNodeField({
+            node,
+            name: `slug`,
+            value: `/posts${relativePath}`,
+        })
+    }
+}
+
 exports.createPages = async ({ actions, graphql, reporter }) => {
     const { createPage } = actions
 
@@ -12,13 +34,14 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
     const result = await graphql(`
         {
             allMarkdownRemark(
-                sort: { order: DESC, fields: [frontmatter___date] }
+                sort: { order: DESC, fields: [fields___date] }
                 limit: 1000
             ) {
                 edges {
                     node {
-                        frontmatter {
+                        fields {
                             slug
+                            date
                         }
                     }
                 }
@@ -34,11 +57,12 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
 
     result.data.allMarkdownRemark.edges.forEach(({ node }) => {
         createPage({
-            path: node.frontmatter.slug,
+            path: node.fields.slug,
             component: blogPostTemplate,
             context: {
                 // additional data can be passed via context
-                slug: node.frontmatter.slug,
+                slug: node.fields.slug,
+                date: node.fields.date,
             },
         })
     })
